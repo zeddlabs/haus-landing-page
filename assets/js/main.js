5a603d4954ec047afa2f08f09c5f76285d824d08
Mhd. Zulfikar Pinem <75366845+ijuldev@users.noreply.github.com>
2021-04-29 10:54:14 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,27 +18,3 @@ function navAction() {
 navLink.forEach(n => {
 	n.addEventListener('click', navAction)
 });
-
-// Scroll Reveal
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
-
-sr.reveal('.home-title', {})
-sr.reveal('.home-text', {delay: 200})
-sr.reveal('.home-img', {delay: 400})
-sr.reveal('.mulai-more', {})
-
-sr.reveal('.section-title', {})
-sr.reveal('.section-text', {delay: 200})
-sr.reveal('.img-container', {})
-
-sr.reveal('.card', {delay: 200})
-sr.reveal('.dftr-rumah-btn', {})
-
-sr.reveal('.product-card', {delay: 200})
-
-sr.reveal('.subcription-box', {delay: 200})
