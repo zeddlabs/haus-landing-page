12e0a2a14f1656f7a2b2ec6eba1a004e07303470
Mhd. Zulfikar Pinem <ijuldev69@gmail.com>
2021-04-29 08:44:01 +0700

    :pencil: Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,17 @@ const navMenu = document.querySelector('.nav-menu')
 navToggle.addEventListener('click', () => {
 	navMenu.classList.toggle('show')
 })
+
+const navLink = document.querySelectorAll('.nav-link')
+
+// Nav Link Action
+function navAction() {
+	navLink.forEach(n => n.classList.remove('active'))
+
+	this.classList.add('active')
+	navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => {
+	n.addEventListener('click', navAction)
+});
